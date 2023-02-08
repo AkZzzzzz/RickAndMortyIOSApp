a57c628c43f158f33cb6aa6c429f445696c9c59f
Artem Kozlovskiy <artem.kozlovskiy@icloud.com>
2023-02-08 12:34:21 +0300

    Added character list view

--- RickAndMorty/Controllers/Core/RSCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RSCharacterViewController.swift
@@ -8,23 +8,24 @@
 import UIKit
 
 final class RSCharacterViewController: UIViewController {
+    
+    private let characterListView = CharacterListView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
         title = "Characters"
-        
-        RSService.share.execute(.listCharactersRequest, expecting: RSGetAllCharactersResponse.self) { result in
-            switch result {
-            case .success(let model):
-                print("Total: "+String(model.info.count))
-                print("Paage result count: "+String(model.results.count))
-
-            case .failure(let error):
-                print(String(describing: error))
-            }
-        }
+        setUpView()
    }
    
+    private func setUpView() {
+        view.addSubview(characterListView)
+        NSLayoutConstraint.activate([
+            characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            characterListView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            characterListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            characterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
 }
